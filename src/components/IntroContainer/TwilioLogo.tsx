ffa4f171364376ff693b77ae1b2b7feed1ad0760
BisiDavi <oludavidconnect@gmail.com>
2021-02-15 12:28:32 +0100

    readme

--- src/components/IntroContainer/TwilioLogo.tsx
+++ src/components/IntroContainer/TwilioLogo.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Applogo } from '../../imports';
 
-export default function TwilioLogo({ className }) {
+export default function TwilioLogo({ className }:any) {
   return <img className={className} src={Applogo} alt="logo" />;
 }
