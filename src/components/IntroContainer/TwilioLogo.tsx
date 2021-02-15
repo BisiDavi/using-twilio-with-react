f3b83d1c3351fa5f142b5834ae436acd09b516c5
BisiDavi <oludavidconnect@gmail.com>
2021-02-15 11:44:59 +0100

    twilio demo react

---
+++ src/components/IntroContainer/TwilioLogo.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Applogo } from '../../imports';
+
+export default function TwilioLogo({ className }) {
+  return <img className={className} src={Applogo} alt="logo" />;
+}
---
+++ src/imports/index.ts
@@ -0,0 +1,1 @@
+export { default as Applogo } from '../images/daytranslationslogo.svg';
